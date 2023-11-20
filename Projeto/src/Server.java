d832e8f1314b0725568a8259a24b67caad8cf4eb
LuisFilipe6 <luis-filipe2001@live.com.pt>
2023-11-20 17:54:45 +0000

    Update Server.java

--- Projeto/src/Server.java
+++ Projeto/src/Server.java
@@ -1,3 +1,6 @@
+import sd23.JobFunction;
+import sd23.JobFunctionException;
+
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -10,6 +13,17 @@ public class Server implements Runnable {
 
     @Override
     public void run() {
+        try {
+            // obter a tarefa de ficheiro, socket, etc...
+            byte[] job = new byte[1000];
 
+            // executar a tarefa
+            byte[] output = JobFunction.execute(job);
+
+            // utilizar o resultado ou reportar o erro
+            System.err.println("success, returned "+output.length+" bytes");
+        } catch (JobFunctionException e) {
+            System.err.println("job failed: code="+e.getCode()+" message="+e.getMessage());
+        }
     }
 }
